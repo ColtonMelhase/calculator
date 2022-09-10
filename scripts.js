639fb95193b3c1f929f1a6734a04d14fdd4bb4ae
Colton <coltonmelhase12@gmail.com>
2022-09-10 12:08:18 -0700

    Add number functionality with display

--- scripts.js
+++ scripts.js
@@ -1,3 +1,9 @@
+let previous = 0;
+let current = 0;
+
+const currentNumber = document.querySelector(".currentNumber");
+const previousNumber = document.querySelector(".previousNumber");
+
 function add(a, b) {
     return a + b;
 }
@@ -14,4 +20,18 @@ function divide(a, b) {
     return a / b;
 }
 
-function operate()
+
+function pressNumber(a) {
+
+    //'tack' on pressed number to number string
+    let tack = '' + current + a;
+    current = parseInt(tack);
+
+    //push current to currentNumber display
+    currentNumber.textContent = "";
+    currentNumber.insertAdjacentText('beforeend', current); 
+}
+
+function pressOperator(a) {
+
+}
