ec0154c5781da4d1ce3b56f0e2029a8485c0b523
Colton <coltonmelhase12@gmail.com>
2022-09-10 14:54:35 -0700

    Add functionality to operator buttons

--- scripts.js
+++ scripts.js
@@ -1,5 +1,7 @@
 let previous = 0;
 let current = 0;
+let result = 0;
+let operator = "";
 
 const currentNumber = document.querySelector(".currentNumber");
 const previousNumber = document.querySelector(".previousNumber");
@@ -20,7 +22,17 @@ function divide(a, b) {
     return a / b;
 }
 
-
+function operate(symbol, a, b) {
+    if(symbol === "/") {
+        return a / b;
+    } else if (symbol === "x") {
+        return a * b
+    } else if(symbol === "-") {
+        return a - b;
+    } else if(symbol === "+") {
+        return a + b;
+    } 
+}
 function pressNumber(a) {
 
     //'tack' on pressed number to number string
@@ -33,5 +45,39 @@ function pressNumber(a) {
 }
 
 function pressOperator(a) {
+    if(a != '=' && a != 'clear') {
+        operator = a;
+        console.log(operator);
 
+        previous = current;
+        current = "";
+
+        currentNumber.textContent = "";
+        currentNumber.insertAdjacentText('beforeend', current);
+        previousNumber.textContent = "";
+        previousNumber.insertAdjacentText('beforeend', previous);
+        previousNumber.insertAdjacentText('beforeend', operator);
+    } else if(a == '=') {
+        current = operate(operator, previous, current)
+        console.log(current);
+
+        previousNumber.textContent = "";
+        currentNumber.textContent = "";
+        currentNumber.insertAdjacentText('beforeend', current);
+    } else {
+        current = 0;
+        previous = 0;
+        product = 0;
+        operator = "";
+        previousNumber.textContent = "";
+        currentNumber.textContent = "";
+    }
+    
+    
+    
+    
+    
+    
+
+    
 }
